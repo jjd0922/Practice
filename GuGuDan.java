903cea761dd5be02b716263109a9bd2eba102dc2
jjd0922 <95264953+jjd0922@users.noreply.github.com>
2021-11-30 15:32:10 +0900

    Update GuGuDan.java

--- GuGuDan.java
+++ GuGuDan.java
@@ -2,7 +2,8 @@ public class Pracice {
 	public static void main(String[] args) {
 		for(int i= 1; i< 10;i++) {
 			System.out.println(3 +" * " +i +" = " + 3*i );
-			System.out.println("git hub 에서 수정한 내용");
 		}
+		System.out.println("git hub 에서 수정한 내용");
+		System.out.println("git pull 사용해서 가져오기");
 	}
 }
